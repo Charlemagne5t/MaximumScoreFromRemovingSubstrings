9fe864974fb0d8a342870e5281fcb020d9280ff0
alek <wait82985@gmail.com>
2025-07-23 08:01:33 +0500

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,66 +1,47 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-public class Solution {
+class Solution {
     public int maximumGain(String s, int x, int y) {
-        int n = s.length();
         int res = 0;
-        Deque<Character> stack = new ArrayDeque<>();
-        if(y > x) {
-            for(int i = 0; i < n; i++) {
-                char ch = s.charAt(i);
-                if (stack.isEmpty()) {
-                    stack.push(ch);
-                } else {
-                    if (ch == 'a' && stack.peek() == 'b') {
-                        stack.pop();
-                        res += y;
-                    } else {
-                        stack.push(ch);
-                    }
+        if(x > y){
+            Deque<Character> stack = new ArrayDeque<>();
+            for(char ch : s.toCharArray()){
+                if(!stack.isEmpty() && ch == 'b' && stack.peekLast() == 'a' ){
+                    res += x;
+                    stack.removeLast();
+                }else {
+                    stack.addLast(ch);
                 }
             }
-            Deque<Character> stack2 = new ArrayDeque<>();
-            if (!stack.isEmpty()) {
-                stack2.push(stack.pop());
-            }
-            while (!stack.isEmpty()) {
-                if(!stack2.isEmpty()) {
-                    if (stack2.peek() == 'b' && stack.peek() == 'a') {
-                        res+= x;
-                        stack.pop();
-                        stack2.pop();
-                    }else stack2.push(stack.pop());
-                }else stack2.push(stack.pop());
+            Deque<Character> leftovers = new ArrayDeque<>();
+            while(!stack.isEmpty()){
+                char cur = stack.removeLast();
+                if(!leftovers.isEmpty() && cur == 'b' && leftovers.peekLast() == 'a'){
+                    res += y;
+                    leftovers.removeLast();
+                }else leftovers.addLast(cur);
             }
         }else {
-            for(int i = 0; i < n; i++) {
-                char ch = s.charAt(i);
-                if (stack.isEmpty()) {
-                    stack.push(ch);
-                } else {
-                    if (ch == 'b' && stack.peek() == 'a') {
-                        stack.pop();
-                        res += x;
-                    } else {
-                        stack.push(ch);
-                    }
+            Deque<Character> stack = new ArrayDeque<>();
+            for(char ch : s.toCharArray()){
+                if(!stack.isEmpty() && ch == 'a' && stack.peekLast() == 'b' ){
+                    res += y;
+                    stack.removeLast();
+                }else {
+                    stack.addLast(ch);
                 }
             }
-            Deque<Character> stack2 = new ArrayDeque<>();
-            if (!stack.isEmpty()) {
-                stack2.push(stack.pop());
-            }
-            while (!stack.isEmpty()) {
-                if(!stack2.isEmpty()) {
-                    if (stack2.peek() == 'a' && stack.peek() == 'b') {
-                        res+= y;
-                        stack.pop();
-                        stack2.pop();
-                    }else stack2.push(stack.pop());
-                }else stack2.push(stack.pop());
+            Deque<Character> leftovers = new ArrayDeque<>();
+            while(!stack.isEmpty()){
+                char cur = stack.removeLast();
+                if(!leftovers.isEmpty() && cur == 'a' && leftovers.peekLast() == 'b'){
+                    res += x;
+                    leftovers.removeLast();
+                }else leftovers.addLast(cur);
             }
         }
+
         return res;
     }
 }
